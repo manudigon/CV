928761c46805a3ad19cbaf072e577017b05cc739
Nicolas Lavarello <lavarello.nicolas.des@gmail.com>
2023-01-27 00:36:55 -0300

    Primer escritura en el repositorio

---
+++ eventos.js
@@ -0,0 +1,16 @@
+function changeper () {
+    document.getElementById("informacion").innerHTML = "Mi nombre completo es Zoe Sims, mi direccion de email es zoe.sims@gmai.com, mi fecha de nacimiento 12/7/1978, mi direccion 3200 Bollinger Road y mi numero de telefono es (011) 11-2348-4381.";
+}
+
+function changeac () {
+    document.getElementById("informacion").innerHTML = "En el 2012 termine mis estudios secundarios en la Escuela Tecnina nro 32, en 2013 comence mis estudios en Abogacia en la Universidad de Buenos Aires los cuales finalice en el año 2020."
+}
+
+function changexp () {
+    document.getElementById("informacion").innerHTML = "Trabaje durante 2 años en un estudio juridico Forger&Roger, actualmente trabajo por mi cuenta en un estudio juridico que funde con un compañero de la carrera."
+
+}
+
+function changeobj () {
+    document.getElementById("informacion").innerHTML = "Mis objetivos en esta empresa son poder tener un crecimiento personal y economico, ademas de ampliar mis horizontes en nuevos ambitos."
+}
